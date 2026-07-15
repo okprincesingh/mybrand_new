0fafd19be20aa57d848377d68148648e3c156525
Jaikvik Technology <okprincesingh@gmail.com>
2026-07-15 11:06:13 +0530

    Create slug-router.php

---
+++ slug-router.php
@@ -0,0 +1,22 @@
+<?php
+require_once __DIR__ . '/includes/helpers.php';
+require_once __DIR__ . '/includes/catalog.php';
+
+$slug = rawurldecode(trim((string) ($_GET['slug'] ?? ''), "/ \t\n\r\0\x0B"));
+
+if (!validate_slug_value($slug)) {
+    http_response_code(404);
+    require __DIR__ . '/404.php';
+    exit;
+}
+
+$category = catalog_find_category($slug);
+if ($category) {
+    $_GET['category'] = (string) ($category['slug'] ?? $slug);
+    unset($_GET['slug']);
+    require __DIR__ . '/shop.php';
+    exit;
+}
+
+$_GET['slug'] = $slug;
+require __DIR__ . '/why-page.php';
